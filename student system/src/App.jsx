9c7a76c9a8a31039fe78c29d8970566de3b539b4
Lit-23 <rommelmarquez143@gmail.com>
2023-12-05 19:46:23 -0800

    init repo

---
+++ student system/src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-success'>👨‍🎓Student Records</h1>
+    </>
+  )
+}
+
+export default App
